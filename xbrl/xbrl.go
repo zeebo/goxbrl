b1aae3bc8f2b1385c7465207fd8744b677244edf
jeff <leterip@me.com>
2012-07-19 19:50:26 -0400

    added some xbrl data structures

---
+++ xbrl/xbrl.go
@@ -0,0 +1,78 @@
+package xbrl
+
+import (
+	"encoding/xml"
+	"time"
+)
+
+type Company struct {
+	CIK    string
+	URL    string
+	Ticker string
+}
+
+type ContextPeriod int
+
+const (
+	ContextPeriodDuration ContextPeriod = iota
+	ContextPeriodInstant
+)
+
+type Context struct {
+	Start time.Time
+	End   time.Time
+}
+
+func (c *Context) PeriodType() ContextPeriod {
+	if c.End.IsZero() {
+		return ContextPeriodInstant
+	}
+	return ContextPeriodDuration
+}
+
+type Unit struct {
+	Name    string
+	Measure string
+}
+
+type Calc struct {
+	Mul  int
+	Fact *Fact
+}
+
+type Fact struct {
+	Name        xml.Name
+	LabelText   string
+	Href        string
+	Title       string
+	Calculation []*Calc
+}
+
+type Datum struct {
+	Fact  *Fact
+	Unit  *Unit
+	Value string
+}
+
+type Item struct {
+	Datum    *Datum
+	Children Items
+}
+
+type Items []*Item
+
+type Data struct {
+	Context *Context
+	Items   Items
+}
+
+type Chart struct {
+	Title string
+	Data  []*Data
+}
+
+type Filing struct {
+	Charts  []*Chart
+	Date    time.Time
+	Company *Company
+}
---
+++ xbrl/xbrl_test.go
@@ -0,0 +1,63 @@
+package xbrl
+
+import (
+	"encoding/xml"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func MustParse(a, b string) time.Time {
+	t, err := time.Parse(a, b)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func TestNothing(t *testing.T) {
+	company := &Company{
+		CIK:    "0000843006",
+		URL:    "http://issuerdirect.com/",
+		Ticker: "isdr",
+	}
+
+	c1 := &Context{
+		Start: MustParse("2006 01 02", "2009 12 31"),
+	}
+	u1 := &Unit{
+		Name:    "USD",
+		Measure: "iso4271:USD",
+	}
+
+	f1 := &Fact{Name: xml.Name{"isdr", "AssetsCurrent"}}
+	f2 := &Fact{Name: xml.Name{"isdr", "OtherAssetsCurrent"}}
+	f3 := &Fact{
+		Name:        xml.Name{"isdr", "Assets"},
+		Calculation: []*Calc{{1, f1}, {1, f2}},
+	}
+
+	//tree for first context
+	data1 := &Data{
+		Context: c1,
+		Items: Items{
+			{&Datum{f3, u1, "465005"}, Items{
+				{&Datum{f1, u1, "323555"}, nil},
+				{&Datum{f2, u1, "19201"}, nil},
+			}},
+		},
+	}
+
+	c := &Chart{
+		Title: "DocumentSomethingOrOther",
+		Data:  []*Data{data1},
+	}
+
+	f := Filing{
+		Charts:  []*Chart{c},
+		Date:    time.Now(),
+		Company: company,
+	}
+
+	fmt.Printf("%#v\n", f)
+}
